5b5d92e07646848132ea776e1ebf51ca4202dabc
PetraSpirkova <petra.spirkova@n26.com>
2020-01-24 17:09:44 +0100

    add first iteration

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout'
+
+
+export default () => (
+  <Layout>
+    <h1>Home</h1>
+    <p>Hello NYC!</p>
+    <Link to="/about/">Learn about me &rarr;</Link>
+  </Layout>
+)
